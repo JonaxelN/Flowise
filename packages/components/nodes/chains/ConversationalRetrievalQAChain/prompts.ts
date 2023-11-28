32083d805a1a22310abcd4ee72bd81b62b6c9a3b
JonaxelN <jonathanaxelnunez@gmail.com>
2023-11-28 12:37:36 -0600

    Change prompts

--- packages/components/nodes/chains/ConversationalRetrievalQAChain/prompts.ts
+++ packages/components/nodes/chains/ConversationalRetrievalQAChain/prompts.ts
@@ -1,16 +1,16 @@
-export const default_qa_template = `Use the following pieces of context to answer the question at the end. If you don't know the answer, just say that you don't know, don't try to make up an answer.
+export const default_qa_template = `Utiliza las siguientes piezas de contexto para responder la pregunta al final. Si no sabes la respuesta, simplemente di que no lo sabes, no intentes inventar una respuesta.
 
 {context}
 
-Question: {question}
-Helpful Answer:`
+Pregunta: {question}
+Respuesta En Español:`
 
-export const qa_template = `Use the following pieces of context to answer the question at the end.
+export const qa_template = `Utiliza las siguientes piezas de contexto para responder la pregunta al final.
 
 {context}
 
-Question: {question}
-Helpful Answer:`
+Pregunta: {question}
+Respuesta En Español:`
 
 export const default_map_reduce_template = `Given the following extracted parts of a long document and a question, create a final answer. 
 If you don't know the answer, just say that you don't know. Don't try to make up an answer.
@@ -56,9 +56,9 @@ We have the opportunity to refine the existing answer (only if needed) with some
 Given the new context, refine the original answer to better answer the question. 
 If you can't find answer from the context, return the original answer.`
 
-export const CUSTOM_QUESTION_GENERATOR_CHAIN_PROMPT = `Given the following conversation and a follow up question, rephrase the follow up question to be a standalone question, answer in the same language as the follow up question. include it in the standalone question.
+export const CUSTOM_QUESTION_GENERATOR_CHAIN_PROMPT = `Dado el siguiente diálogo y una pregunta de seguimiento, reformula la pregunta de seguimiento para que sea una pregunta independiente, responde en el mismo idioma que la pregunta de seguimiento e incluye eso en la pregunta independiente.
 
 Chat History:
 {chat_history}
-Follow Up Input: {question}
-Standalone question:`
+Pregunta de Seguimiento: {question}
+Pregunta Independiente:`
